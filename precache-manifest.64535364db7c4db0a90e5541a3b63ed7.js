7b3a58898fba4b39154703c4865f1c8104b840f5
Ethan Vernon <ethan.vernon@colorado.edu>
2019-02-10 10:58:32 +0800

    Updates

---
+++ precache-manifest.64535364db7c4db0a90e5541a3b63ed7.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = [
+  {
+    "revision": "05e4eb29e452cf6e677e3d2bc2d18e54",
+    "url": "/portfolio2/static/media/thumbnail-pomo.05e4eb29.png"
+  },
+  {
+    "revision": "11cf0fb3cb2c4e920a2b",
+    "url": "/portfolio2/static/css/main.f7944b95.chunk.css"
+  },
+  {
+    "revision": "416f1532f1fcac5b23abb4b37db51e65",
+    "url": "/portfolio2/static/media/codepen-brands.416f1532.svg"
+  },
+  {
+    "revision": "6915cbf39c0296a28b51",
+    "url": "/portfolio2/static/js/1.6915cbf3.chunk.js"
+  },
+  {
+    "revision": "099cc4b1c2dc80af7879",
+    "url": "/portfolio2/static/js/runtime~main.099cc4b1.js"
+  },
+  {
+    "revision": "356b6fb24f4724bed76073c9680a92d0",
+    "url": "/portfolio2/static/media/code-icon.356b6fb2.svg"
+  },
+  {
+    "revision": "588a78b94f0049a41c8e0009590cb27f",
+    "url": "/portfolio2/static/media/logo.588a78b9.svg"
+  },
+  {
+    "revision": "8a92f0a96371b031a403502a610bb2ff",
+    "url": "/portfolio2/static/media/myAvatar.8a92f0a9.svg"
+  },
+  {
+    "revision": "896c31488316b18557a8e50a667b3c8f",
+    "url": "/portfolio2/static/media/thumbnail-dice.896c3148.png"
+  },
+  {
+    "revision": "11cf0fb3cb2c4e920a2b",
+    "url": "/portfolio2/static/js/main.11cf0fb3.chunk.js"
+  },
+  {
+    "revision": "3dc5eb54a479534005765851f0651cf3",
+    "url": "/portfolio2/static/media/thumbnail-npc.3dc5eb54.png"
+  },
+  {
+    "revision": "0aba414073a3ba32ba8752c28ee9d911",
+    "url": "/portfolio2/static/media/thumbnail-party.0aba4140.png"
+  },
+  {
+    "revision": "302a55c86853a7c60bf5b6ba75fe86a7",
+    "url": "/portfolio2/static/media/thumbnail-ab.302a55c8.png"
+  },
+  {
+    "revision": "fdde09cc0e927b54c04b86848725f48b",
+    "url": "/portfolio2/static/media/thumbnail-spell.fdde09cc.png"
+  },
+  {
+    "revision": "d1259f8cc8dc449f043b18a42b224ee1",
+    "url": "/portfolio2/static/media/logo-invert.d1259f8c.svg"
+  },
+  {
+    "revision": "8c0e426a91abdde421f2171ab9d64cc7",
+    "url": "/portfolio2/static/media/linkedin-in-brands.8c0e426a.svg"
+  },
+  {
+    "revision": "c58fda8eab8073d791d52dd27a9b7266",
+    "url": "/portfolio2/static/media/github-brands.c58fda8e.svg"
+  },
+  {
+    "revision": "6915cbf39c0296a28b51",
+    "url": "/portfolio2/static/css/1.af1a311e.chunk.css"
+  },
+  {
+    "revision": "004397fe6130c111cd0483af87d3a8b6",
+    "url": "/portfolio2/index.html"
+  }
+];
